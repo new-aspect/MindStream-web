f1e5a5937f1fbe5381484119fa16327b73bb805b
nzhao <nzhao@sudytech.com>
2024-08-29 11:18:21 +0800

    解决后端转发路由的跨域问题

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const {createProxyMiddleware} = require('http-proxy-middleware');
+
+const app = express();
+
+// 静态文件服务
+app.use(express.static('./')); 
+
+// 配置代理
+app.use('/api',createProxyMiddleware({
+    target: 'http://localhost:8080', // 后端服务启动地址
+    changeOrigin: true
+}))
+
+// 设置端口
+const PORT = 5500;//前端服务端口
+app.listen(PORT,()=>{
+    console.log(`前端运行在端口 http://localhost:${PORT}`)
+})
